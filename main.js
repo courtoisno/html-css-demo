6afe632205184330a59dbed489292fa38bd61bd1
noémie courtois <courtoisno@gmail.com>
2016-10-04 18:11:50 +0200

    More img

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 $(document).ready(function(){
-	console.log ('hello helooo my friend!')
-$( '#me' ).fadeOut(500)
-$('#me').fadeIn(500)
+	console.log ('DOM has loaded')
+
+	$( '#me' ).fadeOut(500)
+	$('#me').fadeIn(500)
 
 
+	$( "#contact" ).click(function() {
+	  $( "#contactmore" ).slideToggle( "fast" );
+	});
 
-
-
-$( "#contact" ).click(function() {
-  $( "#contactmore" ).slideToggle( "fast" );
-});
-
-$('#about').click( function () {
-	$ ("#contentabout").slideToggle ("fast");
-});
-
-
+	$('#about').click( function () {
+		$ ("#contentabout").slideToggle ("fast");
+	});
 })
